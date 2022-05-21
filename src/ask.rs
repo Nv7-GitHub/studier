ae6d43b5d999d87b2b94c7e77bc247b805a7d69d
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-05-20 19:06:19 -0700

    List question support

--- src/ask.rs
+++ src/ask.rs
@@ -27,8 +27,48 @@ fn ask_normal(q: &String, answer: &String) -> bool {
   cmd::input() == "y"
 }
 
-fn ask_multiple(q: &String, answers: &Vec<String>) -> bool {
+fn print_list_text(q: &String, answers: &Vec<String>) {
   progress();
+  println!("{} {}", cmd::primary(q), cmd::secondary(&format!("[{} answers]", answers.len())));
+}
+
+fn ask_multiple(q: &String, answers: &Vec<String>) -> bool {
+  let mut ans: Vec<String> = Vec::new();
+  let mut finished: Vec<bool> = vec![false; answers.len()];
+  while ans.len() < answers.len() {
+    print_list_text(q, answers);
+    for answer in ans.iter() {
+      println!("{}", answer);
+    }
+    eprint!("\n{} ", cmd::secondary(&"Answer:".to_string()));
+
+    // Check if input is correct
+    let inp = cmd::input();
+    let mut ind = -1;
+    for (i, val) in answers.iter().enumerate() {
+      if val.to_lowercase() == inp {
+        ind = i as i32;
+        break;
+      }
+    }
+    if ind == -1 {
+      // Wrong
+      print_list_text(q, answers);
+      println!("\n{}", cmd::correct(&"Remaining Answers:".to_string()));
+      for (i, v) in answers.iter().enumerate() {
+        if !finished[i] {
+          println!("{}", v);
+        }
+      }
+      println!("\n{} {}", cmd::wrong(&"Your Answer:".to_string()), inp);
+      eprint!("\nPress ENTER to continue...");
+      cmd::input();
+      return false;
+    }
+
+    finished[ind as usize] = true;
+    ans.push(inp);
+  }
   true
 }
 
